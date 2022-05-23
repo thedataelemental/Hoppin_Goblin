0bed2936914cdb213cbc8da2e51194186f28156d
Data <95385516+thedataelemental@users.noreply.github.com>
2022-05-23 11:22:23 -0700

    Add files via upload

--- Hoppin Goblin/hoppin_goblin.py
+++ Hoppin Goblin/hoppin_goblin.py
@@ -25,6 +25,14 @@ class Obstacle:
 		self.image = image
 		self.x = x
 		self.y = y
+		self.collided = False
+		
+# Decorative plants on the ground
+class Plant:
+	def __init__(self, image, x, y):
+		self.image = image
+		self.x = x
+		self.y = y
 		
 
 # Start pygame window
@@ -34,7 +42,6 @@ screen = pygame.display.set_mode((960, 480), \
 pygame.display.set_caption("HOPPIN GOBLIN")
 # CCC_icon = pygame.image.load("Assets/Exports/CCC.ico")
 # pygame.display.set_icon(CCC_icon)
-
 
 
 # Import assets
@@ -52,17 +59,28 @@ goblin_img_4 = pygame.image.load\
 	
 goblin_images = [goblin_img_1, goblin_img_2, goblin_img_3, goblin_img_4]
 
-knight_image = pygame.image.load\
-	("Assets/Exports/knight.png")
+knight_image = pygame.image.load("Assets/Exports/knight.png")
+
+collision_img = pygame.image.load("Assets/Exports/collision.png")
+
+flower_img = pygame.image.load("Assets/Exports/flower.png")
+
+
 
 # Create goblin player and knight obstacles
 player = Goblin(goblin_images, 120, 320)
 knight_1 = Obstacle(knight_image, 1000, 368)
-knight_2 = Obstacle(knight_image, 1200, 368)
-knight_3 = Obstacle(knight_image, 1400, 368)
+knight_2 = Obstacle(knight_image, 2000, 368)
+knight_3 = Obstacle(knight_image, 2500, 368)
 knights = [knight_1, knight_2, knight_3]
 
+# Create decorative plants
+flower = Plant(flower_img, 1100, 400)
+plants = [flower,]
+
 # Define starting conditions
+text_font = pygame.font.Font("Assets/NESfont.ttf", 28)
+WHITE = (255, 255, 255)
 score_counter = 0
 frame_counter = 0
 speed = 5
@@ -71,6 +89,8 @@ clock = pygame.time.Clock()
 jumping = False
 jump_counter = -4
 
+
+# Main game loop
 while collision == False:
 	# Tell pygame not to freeze
 	pygame.event.pump()
@@ -94,6 +114,7 @@ while collision == False:
 	events = pygame.event.get()
 	for event in events:
 		if event.type == pygame.KEYDOWN:
+			# Player hits jump button
 			if event.key == pygame.K_SPACE:
 				jumping = True
 					
@@ -102,40 +123,77 @@ while collision == False:
 			sys.exit()
 	
 	
-	# WIP zone
-##############################################################
-
-	# Make goblin jump
-	
-	print(jumping)
-	print(jump_counter) 
-	
+	# Make goblin jump #
+	# Track jump's intended path
 	if jumping == True:
-		if (-4 <= jump_counter <= 6):
-			jump_counter += 1
-	if jump_counter >= 5:
+		if (-4 <= jump_counter <= 4):
+			jump_counter += 0.2
+			
+	if jump_counter >= 4:
 		jumping = False
 		jump_counter = -4
 	
-	player.y = 192 + (8 * (jump_counter * jump_counter))
+	# Change goblin's height
+	player.y = 128 + (12 * (jump_counter * jump_counter))
 	
-	# Implement this without using the for loop:
+#	The above code implements this without using the for loop:
 #	for x in range (-4, 5):
-#		player.y = -(x * x) + 16
-#		print(y)
+#		player.y = 128 + (12 * (x * x))
 
-#############################################################
-	
 	
 	# Render screen
 	screen.blit(background_img, (0,0))
 	screen.blit(player.current_image, (player.x, player.y))
+	
+	# Move enemy knights
 	for knight in knights:
 		knight.x -= speed
+		
+	# Check for collision beween goblin and knights
+	for knight in knights:
+		if (((abs((player.x) - knight.x) < 64)) and \
+			((abs((player.y + 48) - knight.y)) < 8)):
+				screen.blit(collision_img, (250, 200))
+				score_counter = 0
+				knight.collided = True
+		
+		# Reset knights at right side of screen after passing thru
 		if knight.x <= -80:
-			knight.x = 1000
+			knight.x = 2500
+			if knight.collided == False:
+				score_counter += 1
+			else:
+				knight.collided = False
+			print(score_counter)
 		screen.blit(knight.image, (knight.x, knight.y))
+	
+#	# Move and draw decorative plants
+#	for plant in plants:
+#		plant.x -= speed
+#		if plant.x <= -80:
+#			plant.x = 1200
+#		screen.blit(plant.image, (plant.x, plant.y))
+				
+	score_display = text_font.render("SCORE: " + str(score_counter), True, WHITE)
+	screen.blit(score_display, (400, 200))
+		
 	pygame.display.flip()
 	clock.tick(60)
+
+# TODO:
+# Randomize guard spawns.
+# Fix running animation.
+# Add more detail to the ground.
+
+# Guard spawn randomization:
+# Determine guard spawning location based on the spawn loc of prev guard.
+# Distance from previous guard is randomized within a min and max value.
+# The range of possible distance shrinks as the Score increases.
+# Distance should always be at least 200 units.
+
+
+
+
+
 
 
